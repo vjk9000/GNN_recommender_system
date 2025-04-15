d2715a0200600b65f9b66d9b26c30e15a1e8a720
Xinyi <xinzwerkz@gmail.com>
2025-04-15 12:14:02 +0800

    remove init params in gs wrapper and move to gs py

--- embedding_gs_wrapper.py
+++ embedding_gs_wrapper.py
@@ -12,21 +12,18 @@ from utils.setup_embeddings import e5_embedding_model, custom_BLaIR_text_embeddi
 
 
 # change the parent class later
-class EmbeddingGSWrapper(BaseEstimator, RegressorMixin):
-    def __init__(self, embedding_model_name="intfloat/e5-small-v2", pooling="cls", batch_size=64, max_length=512, device=None):
+class EmbeddingAndGNNWrapper(BaseEstimator, RegressorMixin):
+    def __init__(self, embedding_model_name="intfloat/e5-small-v2", pooling="cls", batch_size=64, max_length=512, device=None,
+                 edges=None):
         self.embedding_model_name = embedding_model_name
         self.pooling = pooling
         self.batch_size = batch_size
         self.max_length = max_length
         self.device = device or ("cuda" if torch.cuda.is_available() else "cpu")
 
-        self.user_features_numeric_agg = pd.read_parquet("cleaned_v2/user_features_numeric_agg.parquet")
-        self.user_features_string_agg = pd.read_parquet("cleaned_v2/user_features_string_agg.parquet")
-        self.product_features_numeric = pd.read_parquet("cleaned_v2/product_features_numeric.parquet")
-        self.product_features_string = pd.read_parquet("cleaned_v2/product_features_string.parquet")
-        self.train_edges = pd.read_parquet("cleaned_v2/train_edges.parquet")
-        self.test_edges = pd.read_parquet("cleaned_v2/test_edges.parquet")
-        self.val_edges = pd.read_parquet("cleaned_v2/val_edges.parquet")
+        # concat'd features
+        self.user_features = None
+        self.product_features = None
 
         self.user_id_to_idx = pickle.load(open("cleaned_v2/user_id_to_idx.pkl", "rb"))
         self.prod_id_to_idx = pickle.load(open("cleaned_v2/prod_id_to_idx.pkl", "rb"))
@@ -35,36 +32,29 @@ class EmbeddingGSWrapper(BaseEstimator, RegressorMixin):
         self.embedding_size = 64
 
     def fit(self, X, y=None):
-
-        # product string, user string, product num, user num, = X
-        # Unpack inputs
-        # (product_text_series, product_num_feats,
-        #  user_num_feats,
-        #  train_edge_index, train_edge_weights,
-        #  val_edge_index, val_edge_weights,
-        #  y_true, gnn_class, gnn_kwargs) = X
-        #
+        (user_features_numeric_agg, user_features_string_agg, product_features_numeric,
+        product_features_string, train_edges, test_edges, val_edges) = X
 
         # TODO: pass in embedding model name, pooling, max length for GS
 
-        product_meta_embeddings, user_reviews_embeddings = self._embed(self.product_features_string, self.user_features_string_agg,
-                                                                       embedding_model_name, pooling, max_length)
+        product_meta_embeddings, user_reviews_embeddings = self._embed(product_features_string, user_features_string_agg,
+                                                                       self.embedding_model_name, self.pooling, self.max_length)
 
-        product_features, user_features = self._prepare_combined_features(product_meta_embeddings, user_reviews_embeddings,
-                                                                          self.product_features_numeric, self.user_features_numeric_agg)
+        self.product_features, self.user_features = self._prepare_combined_features(product_meta_embeddings, user_reviews_embeddings,
+                                                                          product_features_numeric, user_features_numeric_agg)
 
         num_users = len(self.user_id_to_idx)
         num_products = len(self.prod_id_to_idx)
-        user_feature_dim = user_features.shape[1]
-        product_feature_dim = product_features.shape[1]
+        user_feature_dim = self.user_features.shape[1]
+        product_feature_dim = self.product_features.shape[1]
 
-        train_edge_index = torch.tensor(self.train_edges[["user_idx", "prod_idx"]].to_numpy().T, dtype=torch.long)
-        val_edge_index = torch.tensor(self.val_edges[["user_idx", "prod_idx"]].to_numpy().T, dtype=torch.long)
-        self.test_edge_index = torch.tensor(self.test_edges[["user_idx", "prod_idx"]].to_numpy().T, dtype=torch.long)
+        train_edge_index = torch.tensor(train_edges[["user_idx", "prod_idx"]].to_numpy().T, dtype=torch.long)
+        val_edge_index = torch.tensor(val_edges[["user_idx", "prod_idx"]].to_numpy().T, dtype=torch.long)
+        self.test_edge_index = torch.tensor(test_edges[["user_idx", "prod_idx"]].to_numpy().T, dtype=torch.long)
 
-        train_edge_weights = torch.tensor(self.train_edges.rating.to_list(), dtype=torch.float)
-        val_edge_weights = torch.tensor(self.val_edges.rating.to_list(), dtype=torch.float)
-        self.test_edge_weights = torch.tensor(self.test_edges.rating.to_list(), dtype=torch.float)
+        train_edge_weights = torch.tensor(train_edges.rating.to_list(), dtype=torch.float)
+        val_edge_weights = torch.tensor(val_edges.rating.to_list(), dtype=torch.float)
+        self.test_edge_weights = torch.tensor(test_edges.rating.to_list(), dtype=torch.float)
 
         self.base_gnn_model = BaseGNNRecommender(num_users, num_products, user_feature_dim,product_feature_dim).to(self.device)
         optimizer = torch.optim.Adam(self.base_gnn_model.parameters(), lr=0.01)
@@ -73,7 +63,7 @@ class EmbeddingGSWrapper(BaseEstimator, RegressorMixin):
             self.base_gnn_model,
             train_edge_index, train_edge_weights,
             val_edge_index, val_edge_weights,
-            user_features, product_features,
+            self.user_features, self.product_features,
             num_epochs=5,
             print_progress=False
         )
@@ -84,21 +74,18 @@ class EmbeddingGSWrapper(BaseEstimator, RegressorMixin):
         # self.y_true_ = y_true
         return self
 
-    def predict(self, X):
-        # Your custom predict logic here
-        raise NotImplementedError("Predict method not needed for GridSearch in this case")
-
-    def score(self, X, y=None):
-        # TODO: see how to return rmse for final_evaluation
+    def score(self, X=None, y=None):
         rmse = final_evaluation(self.base_gnn_model, self.test_edge_index, self.test_edge_weights,
                                 self.user_features, self.product_features, self.best_model)
         return rmse
 
     def _embed(self, product_features_string, user_features_string_agg,
                embedding_model_name, pooling, max_length):
+
         product_meta_features, user_review_features = None, None
 
         if embedding_model_name == 'E5':
+            # TODO: meta col + 1/2 of other cols
             product_meta_features = e5_embedding_model(product_features_string["meta"], batch_size=64,
                                                        max_length=max_length, pooling=pooling)
             torch.save(product_meta_features, f"./e5_pdt_meta_{pooling}_{max_length}.pt")
@@ -139,3 +126,5 @@ class EmbeddingGSWrapper(BaseEstimator, RegressorMixin):
 
         return product_features, user_features
 
+
+#scikitlearn have gpu support now? can try, dont need only use cpu
--- embeddings/gridsearch.py
+++ embeddings/gridsearch.py
@@ -6,65 +6,47 @@ from torch.nn.functional import normalize
 from tqdm import tqdm
 from transformers import AutoModel, AutoTokenizer, AutoConfig
 
-from utils.embedding_utils import add_sos_and_bos, aggregate_embeddings
+from sklearn.model_selection import GridSearchCV
 
-E5 = "intfloat/e5-small-v2"
+from embedding_gs_wrapper import EmbeddingAndGNNWrapper
 
 
-class GridSearch():
-    def __init__(self):
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "mps")
-        # self.agg_method = 'mean'
-        self.tokenizer = None
-        self.model = None
-        # self.model.eval()
+def gs_embeddings():
+    user_features_numeric_agg = pd.read_parquet("cleaned_v2/user_features_numeric_agg.parquet")
+    user_features_string_agg = pd.read_parquet("cleaned_v2/user_features_string_agg.parquet")
+    product_features_numeric = pd.read_parquet("cleaned_v2/product_features_numeric.parquet")
+    product_features_string = pd.read_parquet("cleaned_v2/product_features_string.parquet")
 
-    def embed(self):
-        meta_features = torch.load("meta_features_512.pt")
-        review_features = torch.load("review_features_512.pt")
-        user_review_features = torch.load("./user_review_features_512.pt")
+    train_edges = pd.read_parquet("cleaned_v2/train_edges.parquet")
+    test_edges = pd.read_parquet("cleaned_v2/test_edges.parquet")
+    val_edges = pd.read_parquet("cleaned_v2/val_edges.parquet")
 
-    def embed_text(self, hf_model_name, texts, batch_size=32):
-        local_model_name = hf_model_name.replace("/", "_")
-        # print(os.path.exists(f"./{local_model_name}.pt"))
-        if os.path.exists(f"./{local_model_name}.pt"):
-            return pd.read_csv(f"./{local_model_name}.csv").meta
-        self.tokenizer = AutoTokenizer.from_pretrained(hf_model_name)
-        self.model = AutoModel.from_pretrained(hf_model_name).to(self.device)
+    X_custom = (
+        user_features_numeric_agg,
+        user_features_string_agg,
+        product_features_numeric,
+        product_features_string,
+        train_edges,
+        test_edges,
+        val_edges)
 
-        embeddings = []
+    # will be passed into init of wrapper
+    param_grid = {
+        "pooling": ["cls", "max"],
+        "max_length": [2],
+        "embedding_model_name": "E5"
+    }
 
-        with torch.no_grad():
-            for i in tqdm(range(0, len(texts), batch_size)):
-                batch = texts[i:i + batch_size].tolist()
-                inputs = self.tokenizer(batch, return_tensors="pt", padding=True, truncation=True).to(self.device)
-                outputs = self.model(**inputs)
-                emb = normalize(outputs.last_hidden_state[:, 0], p=2, dim=1)
-                embeddings.append(emb)
+    grid = GridSearchCV(
+        EmbeddingAndGNNWrapper(),
+        param_grid=param_grid,
+        scoring=None,  # uses "score" method in class by default
+        cv=[(slice(None), slice(None))],  # dummy CV, since train/val is inside
+        verbose=3
+    )
 
-        return torch.cat(embeddings, dim=0)
-
-    def get_embeddings(self, model_name, product_df, user_df):
-        if model_name == E5:
-            product_df["formatted"] = "passage: " + product_df['meta'].astype(str)
-            # adjust later to correct col
-            # user_df["formatted"] = "passage: " + user_df['meta'].astype(str)
-            product_df["embedding"] = list(gs.embed_text(model_name, product_df["formatted"]))
-            # user_df["embedding"] = list(gs.embed_text(model_name, user_df["formatted"])
-
-        return product_df, user_df
+    grid.fit([X_custom])
 
 
 if __name__ == "__main__":
-    # models = [BLAIR_BASE, BLAIR_CUSTOM, E5]
-    # for model in models...
-    torch.cuda.empty_cache()
-    gs = GridSearch()
-    print(os.getcwd())
-    product_string_df = pd.read_parquet("../data/cleaned_v2/product_features_string.parquet")
-    user_string_df = pd.read_parquet("../data/cleaned_v2/user_features_string_agg.parquet")
-
-    product_string_df, user_string_df = gs.get_embeddings(E5, product_string_df, user_string_df)
-    # just use meta col first
-    # for e5 use formatted
-    print(product_string_df.embedding)
+  gs_embeddings()
