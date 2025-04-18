5cb7c0d93f8c43be822c2e07a6ca5fdda2b32ca7
viknesh <viknesh97@yahoo.com.sg>
2025-04-18 11:11:25 +0800

    added recall. NDCG canot work

--- utils/predictions.py
+++ utils/predictions.py
@@ -39,31 +39,63 @@ def pretty_print_recomendations(recommended_asins, product_df, col):
     for index, asin in enumerate(recommended_asins, start=1):
         print("Product {}:".format(index), product_df[product_df['parent_asin'] == asin][col].values[0])
 
-def evaluate_recall(model, test_edges, k, batch_size, user_nodes, prod_nodes, product_idx, device = None):
+def get_top_k_preds(model, test_user_idx_ls, k, batch_size, user_nodes, prod_nodes, product_idx, device = None):
     model.eval()
     sol = []
     num_products = len(product_idx)
+    
     if device == None:
         device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    for index in range(0, test_edges.shape[1], batch_size):
-        test_user_ids = test_edges[0][index:index+batch_size]
+    for index in range(0, len(test_user_idx_ls), batch_size):
+        test_user_ids = torch.tensor(test_user_idx_ls[index:index+batch_size], dtype=torch.long, device=device)
         test_size = test_user_ids.shape[0]
-        user_ids_expanded = test_user_ids.unsqueeze(1).expand(-1, num_products)
-        product_ids_expanded = product_idx.unsqueeze(0).expand(test_size, -1)
-        user_ids_flat = user_ids_expanded.reshape(-1)
-        product_ids_flat = product_ids_expanded.reshape(-1)
-        batch_test_edges = torch.stack([user_ids_flat.cpu(), product_ids_flat]).to(device)
-        # batch_combined_egde_index = combine_edges(batch_test_edges)
-        # batch_combined_egde_index = batch_combined_egde_index.to(device)
+        
+        # user_ids_expanded = test_user_ids.unsqueeze(1).expand(-1, num_products)
+        # product_ids_expanded = product_idx.unsqueeze(0).expand(test_size, -1)
+        # user_ids_flat = user_ids_expanded.reshape(-1)
+        # product_ids_flat = product_ids_expanded.reshape(-1)
+        # batch_test_edges = torch.stack([user_ids_flat.cpu(), product_ids_flat]).to(device)
+
+        # Optimised with the help of Chat GPT
+        user_ids_flat = test_user_ids.repeat_interleave(num_products)
+        product_ids_flat = product_idx.repeat(test_size)
+        batch_test_edges = torch.stack([user_ids_flat, product_ids_flat], dim=0)
 
         with torch.no_grad():
             scores_flat = model(batch_test_edges, user_nodes, prod_nodes)
         
         scores = scores_flat.view(test_user_ids.shape[0], num_products)
         topk_scores, topk_indices = torch.topk(scores, k, dim=1)
-        true_item_ids = test_edges[1][index:index+batch_size]
-        recall_hits = (topk_indices == true_item_ids.unsqueeze(1)).any(dim=1).float().cpu().tolist()
-        sol.extend(recall_hits)
+        sol.append(topk_indices)
     
-    return torch.tensor(sol).mean()
+    return sol
+
+# def evaluate_recall(model, test_edges, k, batch_size, user_nodes, prod_nodes, product_idx, device = None):
+#     model.eval()
+#     sol = []
+#     num_products = len(product_idx)
+#     if device == None:
+#         device = "cuda" if torch.cuda.is_available() else "cpu"
+
+#     for index in range(0, test_edges.shape[1], batch_size):
+#         test_user_ids = test_edges[0][index:index+batch_size]
+#         test_size = test_user_ids.shape[0]
+#         user_ids_expanded = test_user_ids.unsqueeze(1).expand(-1, num_products)
+#         product_ids_expanded = product_idx.unsqueeze(0).expand(test_size, -1)
+#         user_ids_flat = user_ids_expanded.reshape(-1)
+#         product_ids_flat = product_ids_expanded.reshape(-1)
+#         batch_test_edges = torch.stack([user_ids_flat.cpu(), product_ids_flat]).to(device)
+#         # batch_combined_egde_index = combine_edges(batch_test_edges)
+#         # batch_combined_egde_index = batch_combined_egde_index.to(device)
+
+#         with torch.no_grad():
+#             scores_flat = model(batch_test_edges, user_nodes, prod_nodes)
+        
+#         scores = scores_flat.view(test_user_ids.shape[0], num_products)
+#         topk_scores, topk_indices = torch.topk(scores, k, dim=1)
+#         true_item_ids = test_edges[1][index:index+batch_size]
+#         recall_hits = (topk_indices == true_item_ids.unsqueeze(1)).any(dim=1).float().cpu().tolist()
+#         sol.extend(recall_hits)
+    
+#     return torch.tensor(sol).mean()
