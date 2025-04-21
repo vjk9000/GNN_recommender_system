976b993c38ad49a6cc08488331248c3f83c3dbc9
viknesh <viknesh97@yahoo.com.sg>
2025-04-21 15:37:09 +0800

    baseline model performance settled

--- utils/setup_nodes.py
+++ utils/setup_nodes.py
@@ -1,4 +1,5 @@
 import torch
+import pandas as pd
 
 def create_edge_lists(df):
     from_node = df["user_idx"].to_list()
@@ -14,3 +15,12 @@ def create_edge_lists(df):
 #     reverse_edge_index = torch.stack([product_indices, user_indices], dim=0)
 #     combined_edge_index = torch.cat([forward_edge_index, reverse_edge_index], dim=1)
 #     return combined_edge_index
+
+def create_user_df(train_df, test_df):
+    additional_test_users = test_df[~test_df.user_id.isin(train_df.user_id)].copy()
+    embedding_shape = test_df["embedding"].iloc[0].shape[0]
+    additional_test_users["embedding"] = list(torch.zeros((len(additional_test_users), embedding_shape)).numpy())
+    additional_test_users["word_count_mean"] = 0
+    additional_test_users.iloc[:, 1:-2] = train_df.iloc[:, 1:-2].median()
+    df = pd.concat([train_df.copy(), additional_test_users.copy()])
+    return df
